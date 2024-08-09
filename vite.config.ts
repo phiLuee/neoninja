f02c422132dbd022cceb17acbbc1c5e3c1200854
firekeeper <philip.maasch@gmail.com>
2024-08-09 08:33:08 +0200

    npm update and added types for path

--- vite.config.ts
+++ vite.config.ts
@@ -1,47 +1,47 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
-import react from '@vitejs/plugin-react'
-import dts from 'vite-plugin-dts'
-import tailwindcss from "tailwindcss"
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     dts({
-      tsconfigPath: './tsconfig.lib.json',
-      include: ['src/components/**/*'],
+      tsconfigPath: "./tsconfig.lib.json",
+      include: ["src/components/**/*"],
       insertTypesEntry: true,
       cleanVueFileName: false,
       copyDtsFiles: true,
       rollupTypes: true,
-      outDir: 'dist'
-    })
+      outDir: "dist",
+    }),
   ],
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/components/index.ts'),
-      name: 'NeoNinja',
-      formats: ['es', 'cjs', 'umd'],
-      fileName: (format) => `neoninja.${format}.js`
+      entry: resolve(__dirname, "src/components/index.ts"),
+      name: "NeoNinja",
+      formats: ["es", "cjs", "umd"],
+      fileName: (format) => `neoninja.${format}.js`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom', 'tailwindcss'],
+      external: ["react", "react-dom", "tailwindcss"],
       output: {
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          tailwindcss: 'tailwindcss'
-        }
-      }
+          react: "React",
+          "react-dom": "ReactDOM",
+          tailwindcss: "tailwindcss",
+        },
+      },
     },
     sourcemap: true,
     emptyOutDir: true,
-    copyPublicDir: false
+    copyPublicDir: false,
   },
   css: {
     postcss: {
       plugins: [tailwindcss],
     },
   },
-})
+});
