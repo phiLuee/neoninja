9faf732ae545a5fd88fdc2d6b48fc2012112d350
firekeeper <philip.maasch@gmail.com>
2024-09-08 14:23:51 +0200

    fixed offcanvas directions

--- packages/base-ui/src/components/Offcanvas/Offcanvas.tsx
+++ packages/base-ui/src/components/Offcanvas/Offcanvas.tsx
@@ -6,13 +6,13 @@ import clsx from "clsx";
 export const Offcanvas = forwardRef<HTMLDivElement, OffcanvasProps>(
   ({ children, placement = "right", show = false }, ref) => {
     const baseStyles =
-      "offcanvas backdrop-blur-lg fixed z-40 h-screen p-4 overflow-y-auto transition-transform bg-white/30 w-80 dark:bg-gray-800/30";
+      "offcanvas backdrop-blur-lg fixed z-40 p-4 overflow-y-auto transition-transform bg-white/30 w-80 dark:bg-gray-800/30";
 
     const placementStyles = {
-      top: "transform -translate-y-full",
-      right: "transform translate-x-full top-0 right-0",
-      bottom: "transform translate-y-full",
-      left: "transform -translate-x-full top-0 left-0",
+      top: "transform -translate-y-full right-0 top-0 left-0 w-screen",
+      right: "transform translate-x-full top-0 right-0  h-screen",
+      bottom: "transform translate-y-full w-screen bottom-0 left-0 right-0",
+      left: "transform -translate-x-full top-0 left-0  h-screen",
     };
 
     const visibilityStyles = show ? "transform-none" : "";
@@ -25,12 +25,7 @@ export const Offcanvas = forwardRef<HTMLDivElement, OffcanvasProps>(
 
     return (
       <>
-        <div
-          ref={ref}
-          id="drawer-example"
-          className={classes}
-          aria-labelledby="drawer-label"
-        >
+        <div ref={ref} className={classes} aria-labelledby="drawer-label">
           {children}
         </div>
       </>
